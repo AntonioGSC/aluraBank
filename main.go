0b7a7410e8d89e69196e5d22ce5246f68930f3f2
AntonioGSC <antoniogabrielagsc@gmail.com>
2021-03-02 18:44:29 -0300

    other methods

--- main.go
+++ main.go
@@ -9,14 +9,31 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-	if podeSacar {
+func (c *ContaCorrente) Sacar(valorDoSaque float64) (string, float64) {
+	if valorDoSaque > 0 && valorDoSaque <= c.saldo {
 		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso"
+		return "Saque realizado com sucesso", c.saldo
 	} else {
-		return "Erro ao sacar"
+		return "Erro ao sacar", c.saldo
 	}
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso", c.saldo
+	} else {
+		return "Erro ao depositar", c.saldo
+	}
+}
+
+func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
+	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
+		c.saldo -= valorDaTransferencia
+		contaDestino.Depositar(valorDaTransferencia)
+		return true
+	}
+	return false
 }
 
 func main() {
@@ -26,15 +43,24 @@ func main() {
 		numeroConta:   123456,
 		saldo:         1250.00}
 
-	fmt.Println(conta.saldo)
+	conta2 := ContaCorrente{"Joe", 222, 654321, 2540.50}
+
+	fmt.Println("=================BANCO===================")
+	fmt.Println("Saldo conta 1:", conta.saldo)
+	fmt.Println("Saldo conta 2:", conta2.saldo)
+	fmt.Println("=========================================")
 	fmt.Println(conta.Sacar(300))
-	fmt.Println(conta.saldo)
-
-	// conta2 := ContaCorrente{"Joe", 222, 654321, 2540.50}
-	// fmt.Println(conta2)
-
-	// var conta3 *ContaCorrente
-	// conta3 = new(ContaCorrente)
-	// conta3.titular = "Cris"
-	// fmt.Println(*conta3)
+	fmt.Println("=========================================")
+	status, valor := conta.Depositar(300)
+	fmt.Println(status)
+	fmt.Println("Seu saldo atual é de:", valor)
+	fmt.Println("=========================================")
+	fmt.Println(conta.Transferir(1000, &conta2))
+	fmt.Println("Saldo conta 1:", conta.saldo)
+	fmt.Println("Saldo conta 2:", conta2.saldo)
 }
+
+// var conta3 *ContaCorrente
+// conta3 = new(ContaCorrente)
+// conta3.titular = "Cris"
+// fmt.Println(*conta3)
