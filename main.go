421c8b2bcc7d7342f2364a9f490bd2e8d6fc9405
AntonioGSC <antoniogabrielagsc@gmail.com>
2021-03-01 23:06:38 -0300

    first method

--- main.go
+++ main.go
@@ -9,6 +9,16 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Erro ao sacar"
+	}
+}
+
 func main() {
 	conta := ContaCorrente{
 		titular:       "Jane",
@@ -16,8 +26,15 @@ func main() {
 		numeroConta:   123456,
 		saldo:         1250.00}
 
-	conta2 := ContaCorrente{"Joe", 222, 654321, 2540.50}
+	fmt.Println(conta.saldo)
+	fmt.Println(conta.Sacar(300))
+	fmt.Println(conta.saldo)
 
-	fmt.Println(conta)
-	fmt.Println(conta2)
+	// conta2 := ContaCorrente{"Joe", 222, 654321, 2540.50}
+	// fmt.Println(conta2)
+
+	// var conta3 *ContaCorrente
+	// conta3 = new(ContaCorrente)
+	// conta3.titular = "Cris"
+	// fmt.Println(*conta3)
 }
