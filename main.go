b3bb4c5f5e3f17ae9d1e369cea4ff6b10b5aca93
AntonioGSC <antoniogabrielagsc@gmail.com>
2021-03-01 22:05:32 -0300

    first struct

--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import "fmt"
 
-func main() {
-	var titular string = "Tony"
-	var numeroAgencia int = 589
-	var numeroConta int = 123456
-	var saldo float64 = 1000.50
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
 
-	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+func main() {
+	conta := ContaCorrente{
+		titular:       "Jane",
+		numeroAgencia: 589,
+		numeroConta:   123456,
+		saldo:         1250.00}
+
+	conta2 := ContaCorrente{"Joe", 222, 654321, 2540.50}
+
+	fmt.Println(conta)
+	fmt.Println(conta2)
 }
