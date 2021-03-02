e781e9c47c3f269930534412c489c7774ab4aef7
AntonioGSC <antoniogabrielagsc@gmail.com>
2021-03-02 18:53:06 -0300

    packages and visibility

---
+++ contas/contaCorrente.go
@@ -0,0 +1,35 @@
+package contas
+
+type ContaCorrente struct {
+	Titular       string
+	NumeroAgencia int
+	NumeroConta   int
+	Saldo         float64
+}
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) (string, float64) {
+	if valorDoSaque > 0 && valorDoSaque <= c.Saldo {
+		c.Saldo -= valorDoSaque
+		return "Saque realizado com sucesso", c.Saldo
+	} else {
+		return "Erro ao sacar", c.Saldo
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.Saldo += valorDoDeposito
+		return "Deposito realizado com sucesso", c.Saldo
+	} else {
+		return "Erro ao depositar", c.Saldo
+	}
+}
+
+func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
+	if valorDaTransferencia < c.Saldo && valorDaTransferencia > 0 {
+		c.Saldo -= valorDaTransferencia
+		contaDestino.Depositar(valorDaTransferencia)
+		return true
+	}
+	return false
+}
--- main.go
+++ main.go
@@ -1,63 +1,32 @@
 package main
 
-import "fmt"
-
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) (string, float64) {
-	if valorDoSaque > 0 && valorDoSaque <= c.saldo {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso", c.saldo
-	} else {
-		return "Erro ao sacar", c.saldo
-	}
-}
-
-func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-	if valorDoDeposito > 0 {
-		c.saldo += valorDoDeposito
-		return "Deposito realizado com sucesso", c.saldo
-	} else {
-		return "Erro ao depositar", c.saldo
-	}
-}
-
-func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
-	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
-		c.saldo -= valorDaTransferencia
-		contaDestino.Depositar(valorDaTransferencia)
-		return true
-	}
-	return false
-}
+import (
+	"banco/contas"
+	"fmt"
+)
 
 func main() {
-	conta := ContaCorrente{
-		titular:       "Jane",
-		numeroAgencia: 589,
-		numeroConta:   123456,
-		saldo:         1250.00}
+	conta := contas.ContaCorrente{
+		Titular:       "Jane",
+		NumeroAgencia: 589,
+		NumeroConta:   123456,
+		Saldo:         1250.00}
 
-	conta2 := ContaCorrente{"Joe", 222, 654321, 2540.50}
+	conta2 := contas.ContaCorrente{"Joe", 222, 654321, 2540.50}
 
 	fmt.Println("=================BANCO===================")
-	fmt.Println("Saldo conta 1:", conta.saldo)
-	fmt.Println("Saldo conta 2:", conta2.saldo)
+	fmt.Println("Saldo conta 1:", conta.Saldo)
+	fmt.Println("Saldo conta 2:", conta2.Saldo)
 	fmt.Println("=========================================")
 	fmt.Println(conta.Sacar(300))
 	fmt.Println("=========================================")
 	status, valor := conta.Depositar(300)
 	fmt.Println(status)
-	fmt.Println("Seu saldo atual é de:", valor)
+	fmt.Println("Seu Saldo atual é de:", valor)
 	fmt.Println("=========================================")
 	fmt.Println(conta.Transferir(1000, &conta2))
-	fmt.Println("Saldo conta 1:", conta.saldo)
-	fmt.Println("Saldo conta 2:", conta2.saldo)
+	fmt.Println("Saldo conta 1:", conta.Saldo)
+	fmt.Println("Saldo conta 2:", conta2.Saldo)
 }
 
 // var conta3 *ContaCorrente
